873eeb792ff290cc3702cf7363ad0bc5650e679d
roshan <roshan.grg364@gmail.com>
2023-08-22 22:09:00 +0545

    new branch push

--- TodoApp/Areas/Todo/ViewModel/TodoIndexViewModel.cs
+++ TodoApp/Areas/Todo/ViewModel/TodoIndexViewModel.cs
@@ -14,6 +14,7 @@
     public class TodoFilterModel
     {
         public string? Title { get; set; } 
+        public string? Title123 { get; set; } 
         public string? Status { get; set; } 
         public string currentUserId { get; set; }
         public DateTime FromDate { get; set; }
